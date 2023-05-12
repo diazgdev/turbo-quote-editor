819434abba55618ffc6c505ddf720ff20db0ac98
Guillermo Díaz <diazgdev@192.168.0.192>
2023-05-12 00:37:54 -0600

    dates

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,6 +2,7 @@ class Quote < ApplicationRecord
   validates :name, presence: true
 
   belongs_to :company
+  has_many :line_item_dates, dependent: :destroy
 
   # after_create_commit -> { broadcast_prepend_later_to 'quotes' }
   # after_update_commit -> { broadcast_replace_later_to 'quotes' }
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  resources :quotes
   devise_for :users
+  resources :quotes
+  resources :quotes do
+    resources :line_item_dates, except: [:index, :show]
+  end
 end
